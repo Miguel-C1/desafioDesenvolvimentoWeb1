1728a5f62457fcac07c68efd20c32532e133936a
Miguel <doxito007@gmail.com>
2022-11-29 07:28:41 -0300

    Começo das funções de persistencia de dados

--- Desafio I Desenvolvimento WEB/app.py
+++ Desafio I Desenvolvimento WEB/app.py
@@ -1,4 +1,20 @@
-from flask import Flask, render_template, url_for
+
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+import funcs
+
+config = funcs.LoadConfig()
+
+app = Flask(__name__)
+app.secret_key = 'super secret key'
+# Conexão ao banco de dados
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = '3306' #Caso a porta seja a padrão, comentar linha.
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'fatec'
+app.config['MYSQL_DB'] = 'desafioWEB'
+
+
 
 app = Flask("__name__")
 
@@ -16,6 +32,9 @@ def quemSomos():
 def contato(): 
     return render_template("contato.html")
 
+@app.route("/gravarContato")
+def gravarContato():
+    return
 
 if __name__ == "__main__":
     app.run(debug=True)
